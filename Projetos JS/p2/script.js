2ea8a1c4622428d184105cd1de845d980da463e4
LuizFN <fellippe.nascimento@gmail.com>
2021-04-10 16:18:55 -0300

    Atualizar

--- Projetos JS/p2/script.js
+++ Projetos JS/p2/script.js
@@ -4,6 +4,7 @@ function verificar() {
     
     var formAno = document.getElementById('txtano')
     var res = document.getElementById('res')
+    var imagem = document.getElementById('foto')
 
     if(Number(formAno.value) > 0 && Number(formAno.value) < ano) {
         var formSex = document.getElementsByName('sex')
@@ -11,23 +12,39 @@ function verificar() {
         var genero = ''
         if(formSex[0].checked) {
             genero = 'Homem'
-            if(idade >= 0 && idade <= 12) {
-                //criança
+            if(idade >= 0 && idade <= 4) {
+                //Bebe
+            } else if(idade > 4 && idade <= 12) {
+                 //Criança
             } else if(idade > 12 && idade <= 19 ) {
-                //adolecente
+                //Adolecente
             } else if(idade > 19 && idade <= 30) {
-                //jovem
+                //Jovem
             } else if(idade > 30 && idade <= 60 ) {
-                //aduto
+                //Aduto
             } else {
-                //idoso
-                var imagem = document.getElementById('foto')
-                imagem.src = "img/idoso.jpg"
+                //Idoso
+                res.innerHTML = `${genero} idoso com ${idade} anos!`
+                imagem.src = 'img/idoso.jpg'
             }
         } else if(formSex[1].checked) {
             genero = 'Mulher'
+            if(idade >= 0 && idade <= 4) {
+                //Bebe
+            } else if(idade > 4 && idade <= 12) {
+                 //Criança
+            } else if(idade > 12 && idade <= 19 ) {
+                //Adolecente
+            } else if(idade > 19 && idade <= 30) {
+                //Jovem
+            } else if(idade > 30 && idade <= 60 ) {
+                //Aduto
+            } else {
+                //Idoso
+                res.innerHTML = `${genero} idosa com ${idade} anos!`
+                imagem.src = 'img/'
+            }
         }
-        res.innerHTML = `${genero} com ${idade} anos!`
     }else {
         window.alert('Erro!')
     }
